de8aca2d33ea3c05b8903f250eff55b98441896b
karthika586 <rankskarthika@gmail.com>
2025-08-06 17:55:47 +0530

    Create Sparse arrays.c

---
+++ Week 1/Sparse arrays.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int n, q;
+    scanf("%d", &n);
+    char strings[n][101];   
+    for(int i = 0; i < n; i++) {
+        scanf("%s", strings[i]);
+    }
+    scanf("%d", &q);
+    char queries[q][101];
+    for(int i = 0; i < q; i++) {
+        scanf("%s", queries[i]);
+    }
+    for(int i = 0; i < q; i++) {
+        int count = 0;
+        for(int j = 0; j < n; j++) {
+            if(strcmp(queries[i], strings[j]) == 0) {
+                count++;
+            }
+        }
+        printf("%d\n", count);
+    }
+    return 0;
+}
